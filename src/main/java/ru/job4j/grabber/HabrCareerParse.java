ec5382d75416df088e5ebcee5af086af3a2d5c36
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-07-25 04:00:18 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -0,0 +1,38 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * @author artem.polschak@gmail.com on 25.07.2022.
+ * @project job4j_grabber
+ */
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = Jsoup.connect(PAGE_LINK);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            String vacancyName = titleElement.text();
+
+            Element dateElement = row.select(".vacancy-card__date").first();
+            String dateText = dateElement.text();
+
+            String link = String.format("%s%s", SOURCE_LINK,
+                    linkElement.attr("href"));
+            System.out.printf("%s %s %s%n", vacancyName, dateText, link);
+        });
+    }
+}
