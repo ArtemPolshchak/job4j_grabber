7e3a5afed636a0affa226bcd40e71c5e26685430
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2022-07-29 22:51:00 +0300

    2.1. Преобразование даты [#289476 #325903]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,6 +1,6 @@
 package ru.job4j.grabber.utils;
 
-import java.time.LocalDateTime;
+import java.time.*;
 
 /**
  * @author artem.polschak@gmail.com on 25.07.2022.
@@ -10,15 +10,7 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String[] parseInfoFromSite = parse.split("\\+");
-        String[] parseDateAndTime = parseInfoFromSite[0].split("T");
-        String[] parseDate = parseDateAndTime[0].split("-");
-        String[] parseTime = parseDateAndTime[1].split(":");
-
-        return LocalDateTime.of(Integer.parseInt(parseDate[0]),
-                Integer.parseInt(parseDate[1]),
-                Integer.parseInt(parseDate[2]), Integer.parseInt(parseTime[0]),
-                Integer.parseInt(parseTime[1]),
-                Integer.parseInt(parseTime[2]));
+        String[] tmp = parse.split("\\+");
+        return LocalDateTime.parse(tmp[0]);
     }
 }
